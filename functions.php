9ff4cbc2c693c68cc8ca40433f9909dc639ae7d1
simolex <simolex@yandex.ru>
2022-06-30 00:15:38 +0300

    подключение стилей и скриптов

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+add_action('wp_enqueue_scripts', 'sparrow_styles');
+add_action('wp_footer', 'sparrow_scripts');
+
+function sparrow_styles()
+{
+   wp_enqueue_style('style', get_stylesheet_uri());
+   wp_enqueue_style('style-default', get_template_directory_uri() . "/assets/css/default.css");
+   wp_enqueue_style('style-layout', get_template_directory_uri() . "/assets/css/layout.css");
+   wp_enqueue_style('style-media', get_template_directory_uri() . "/assets/css/media-queries.css");
+}
+
+function sparrow_scripts()
+{
+   wp_deregister_script('jquery');
+   wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+   wp_enqueue_script('jquery');
+
+   wp_enqueue_script('migrate', get_template_directory_uri() . '/assets/js/jquery-migrate-1.2.1.min.js', ['jquery'], '1.2.1', true);
+   wp_enqueue_script('flexslider', get_template_directory_uri() . '/assets/js/jquery.flexslider.js', ['jquery'], '1.0.0', true);
+   wp_enqueue_script('doubletaptogo', get_template_directory_uri() . '/assets/js/doubletaptogo.js', [], '1.0.0', true);
+   wp_enqueue_script('init', get_template_directory_uri() . '/assets/js/init.js', [], '1.0.0', true);
+}
